6e15e89041e41b8322944b76452a94609b865e8b
NikaMeowRysik <86111412+NikaMeowRysik@users.noreply.github.com>
2023-09-19 17:42:15 +0300

    Update Todo.js

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
    export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
@@ -10,6 +7,7 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
              <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
             <div>
             <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+			<FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
               </div>
           </div>			  
 	 )
